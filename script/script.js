0294675d889f7598a47a56ebad7109d32e977df9
forhad1518 <ahforhad20@gmail.com>
2024-09-23 13:22:50 +0600

    donation adn history toggle added

--- script/script.js
+++ script/script.js
@@ -1,2 +1,32 @@
 // Call from home to blog
 changeHtml('blog-btn','blog.html')
+
+
+// Global Element Area
+const donationButton = document.getElementById('donation-btn')
+const historyButton = document.getElementById('history-btn')
+const showCardContainer = document.getElementById('card-container')
+const showHistoryContainer = document.getElementById('history-container')
+
+
+// history btn evenListener
+document.getElementById('history-btn').addEventListener('click',function(){
+    showHistoryContainer.classList.remove('hidden')
+    showCardContainer.classList.add('hidden')
+
+    donationButton.classList.remove('bg-btnFieldColor', 'border-none')
+    donationButton.classList.add('border')
+
+    historyButton.classList.add('bg-btnFieldColor', 'border-none')
+})
+
+// donation btn evenListener
+document.getElementById('donation-btn').addEventListener('click',function(){
+    showCardContainer.classList.remove('hidden')
+    showHistoryContainer.classList.add('hidden')
+
+    historyButton.classList.remove('bg-btnFieldColor', 'border-none')
+    historyButton.classList.add('border')
+
+    donationButton.classList.add('bg-btnFieldColor', 'border-none')
+})
